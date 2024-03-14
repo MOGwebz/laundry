559656caa050657b8bdd18cfee9c5b3b566510ab
Nicholas Kayz <nicholaskayz2@gmail.com>
2024-03-14 13:06:21 +0300

    Sale order view

--- __manifest__.py
+++ __manifest__.py
@@ -1,24 +1,4 @@
-# -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2019-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Jesni Banu and Nilmar Shereef(odoo@cybrosys.com)
-#
-#    You can modify it under the terms of the GNU AFFERO
-#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#    (AGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
+
 
 {
     'name': 'Laundry Management',
@@ -40,6 +20,7 @@
         'views/config_view.xml',
         'views/laundry_report.xml',
         'views/laundry_label.xml',
+        'views/views.xml'
     ],
     'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
